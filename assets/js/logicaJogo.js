30252c3eaf4765d7650dc1268b0f61a4274be1e2
Larissa Paulino <larissaspaulino@gmail.com>
2021-11-30 11:56:22 -0300

    Condiçao de vitoria - nao pode ser na torre1

--- assets/js/logicaJogo.js
+++ assets/js/logicaJogo.js
@@ -1,9 +1,14 @@
-let containerTorres = document.getElementById('container')
+const containerTorres = document.getElementById('container')
+
+const displayContadorMov = document.createElement('section')
+displayContadorMov.innerText = 'Contador de Movimento: '
+containerTorres.appendChild(displayContadorMov)
 
 let torreUm = document.getElementById('torre1')
 let torreDois = document.getElementById('torre2')
 let torreTres = document.getElementById('torre3')
 
+let contadorClique = 0
 let contadorMovimento = 0
 let contadorChildElem = 0
 
@@ -20,7 +25,8 @@ let larguraDiscoDestino = 0
 
 function torreClicada (e) {
     torre = e.currentTarget
-    contadorMovimento++
+    contadorClique++
+    console.log(contadorClique)
     torreOrigemouDestino()
 }
 
@@ -29,26 +35,43 @@ torreDois.addEventListener ('click', torreClicada)
 torreTres.addEventListener ('click', torreClicada)
 
 function torreOrigemouDestino() {
-    if (contadorMovimento % 2 !== 0) {
+    if (contadorClique % 2 !== 0) {
         torreOrigem = torre
         discoOrigem = torreOrigem.lastElementChild
         larguraDiscoOrigem = discoOrigem.clientWidth
-        console.log(larguraDiscoOrigem)
     } else {
         torreDestino = torre
-        if (torreDestino.childElementCount !== 0)
-        discoDestino = torreDestino.lastElementChild
-        larguraDiscoDestino = discoDestino.clientWidth
-       moverDiscoDeDiv()
-       /* torreDestino.appendChild(discoOrigem)
-       contadorChildElem = torreDestino.childElementCount */
+        if (torreDestino.childElementCount !== 0) {
+            discoDestino = torreDestino.lastElementChild
+            larguraDiscoDestino = discoDestino.clientWidth
+            console.log(larguraDiscoDestino*2)
+        }
+        moverDiscoDeDiv()
+        checkWin()
     }
 }
 
 function moverDiscoDeDiv () {
-   if (larguraDiscoOrigem < larguraDiscoDestino || torreDestino.childElementCount === 0) {
-    torreDestino.appendChild(discoOrigem)
-    contadorChildElem = torreDestino.childElementCount
+    if (larguraDiscoOrigem < larguraDiscoDestino || torreDestino.childElementCount === 0) {
+        torreDestino.appendChild(discoOrigem)
+        contadorChildElem = torreDestino.childElementCount
+        contadorMovimento++
+        displayContadorMov.innerHTML= `Contador de Movimento: ${contadorMovimento}`
    }
 }
+
+function checkWin () {
+    let discoIdTres = torreDestino.firstElementChild.getAttribute('id') //maior
+    let discoIdUm = torreDestino.lastElementChild.getAttribute('id')
+    if (contadorChildElem === 3 && discoIdTres === 'disco3' && discoIdUm === 'disco1' && torreDestino.getAttribute('id') !== 'torre1') {
+        alert('Você venceu!!!')
+        contadorClique = 0 // funcao reset
+        contadorMovimento = 0
+    }
+}
+
+// falta mover o último disco para a torre destino antes do alert de vitória
+// pop up vitoria
+// função reset - appendChild em todas as divs na torre um, e zerar contadores --- e criar botao reset + eventlistener
+
 
